3526d73a654be0d32d023009983e734635a50567
hmoses <hwmoses2@icloud.com>
2023-08-03 16:02:24 -0700

    adding first go project

---
+++ main.go
@@ -0,0 +1,16 @@
+
+package main
+
+import "fmt"
+
+func main() {
+
+
+    var conferenceName = "Go Conference"
+    const conferenceTickets = 50
+    var remainingTickets = 50
+    
+    
+    fmt.Println("Welcome to %v booking application!", conferenceName)
+    fmt.Println("We have a total of %v, tickets and %v, are still available \n", conferenceTickets,remainingTickets)
+    fmt.Println("Get your tickets here to attend")

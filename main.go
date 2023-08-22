64d57798cc7c6663c6d53da9fae9f6a0d03f42c3
Osmosis <hwmoses2@icloud.com>
2023-08-22 14:36:02 -0700

    pushing latest changes

--- main.go
+++ main.go
@@ -14,3 +14,4 @@ func main() {
     fmt.Println("Welcome to %v booking application!", conferenceName)
     fmt.Println("We have a total of %v, tickets and %v, are still available \n", conferenceTickets,remainingTickets)
     fmt.Println("Get your tickets here to attend")
+}
